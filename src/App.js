ffaf9465dbca4f8f0e5d004f0bf4e8f4c817adbe
Sagar ghare <sagar.ghare@acc.ltd>
2023-10-26 12:27:08 +0530

    initaiote commit added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import User from "./Component/User";
+
+const App = () => {
+  const design = (
+    <>
+      <User />
+    </>
+  );
+  return design;
+}
+
+export default App;
---
+++ src/Component/User.jsx
@@ -0,0 +1,10 @@
+const User = () => {
+    const design = (
+        <>
+            <h1>Welcome to the USER COMPONENT.</h1>
+        </>
+    );
+    return design;
+}
+
+export default User;
