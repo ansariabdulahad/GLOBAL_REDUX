816c17d3ca64c402879c4e15da4c92c6737c7035
Sagar ghare <sagar.ghare@acc.ltd>
2023-10-26 17:12:09 +0530

    code completed and understand

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import User from "./Component/User";
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
 
 const App = () => {
+
+  const reducer = (state = "", action) => {
+    if (action.type === 'Testing') {
+      return "SUCCESS"
+    }
+  }
+
+  const store = createStore(reducer);
+
   const design = (
     <>
-      <User />
+      <Provider store={store}>
+        <User />
+      </Provider>
     </>
   );
   return design;
--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,7 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+
 const User = () => {
+    const dispatch = useDispatch();
+    const response = useSelector(response => response);
     const design = (
         <>
+            {
+                JSON.stringify(response)
+            }
             <h1>Welcome to the USER COMPONENT.</h1>
+            <button
+                onClick={() => dispatch({
+                    type: "Testing"
+                })}
+            >Click Me</button>
         </>
     );
     return design;
